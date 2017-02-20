3496be4b3e96c4c504fc4a9bacc04eb9a88168fd
Tevin Aduma <Tevin Aduma>
2017-02-20 21:16:45 +0300

    add loop and unordered list to the webpage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
 
-    
+    var icecreams = ["vanilla", "chilli", "crumbulee", "banana", "strawberry"]
+    icecreams.forEach(function(icecream){
+        alert('I love' + ' ' + icecream + '!');
+
+
+    });
+
+    function makeUL(array) {
+        // Create the list element:
+        var list = document.createElement('ul');
+
+        for(var i = 0; i < array.length; i++) {
+            // Create the list item:
+            var item = document.createElement('li');
+
+            // Set its contents:
+            item.appendChild(document.createTextNode(array[i]));
+
+            // Add it to the list:
+            list.appendChild(item);
+        }
+
+        // Finally, return the constructed list:
+        return list;
+    }
+
+    // Add the contents of options[0] to #foo:
+    document.getElementById('foo').appendChild(makeUL(icecreams[0]));
+
 });
